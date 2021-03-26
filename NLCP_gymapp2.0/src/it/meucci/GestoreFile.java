27d49911488fcc94572b619a25ecad553e71a5a0
NapoliFabian <fabiannapoli3bi@gmail.com>
2021-03-26 11:07:42 +0100

    Creato gestore file

---
+++ NLCP_gymapp2.0/src/it/meucci/GestoreFile.java
@@ -0,0 +1,5 @@
+package it.meucci;
+
+public class GestoreFile {
+
+}
--- NLCP_gymapp2.0/src/it/meucci/Startsl.java
+++ NLCP_gymapp2.0/src/it/meucci/Startsl.java
@@ -1,6 +1,11 @@
 package it.meucci;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Vector;
@@ -38,11 +43,39 @@ public class Startsl extends HttpServlet {
 			int mese =  dat.getMonth()+1;
 			int anno = dat.getYear()+1900;
 			int g=dat.getDate();
+			//InputStream input = getClass().getResourceAsStream("dbconf.txt");
 			String data = anno+"-"+mese+"-"+g;
 			DBManager db = new DBManager();
 			db.aggiornaAbbonamenti(data);
-			System.out.println(System.getProperty("user.dir"));
+			//System.out.println(System.getProperty("user.dir"));
 			UpdateAbbornamenti aggiorna = new UpdateAbbornamenti();
+			String email = null;
+			/*try {
+				URL percorso = getServletContext().geta
+				//String percorso=".config";
+				String file = String.valueOf(percorso);
+				System.out.println(percorso);
+			//File file = new File(percorso+"/dbconf.txt");
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+			email = br.readLine();
+			System.out.println("Nel file ho letto:"+ email);
+			br.close();
+			} catch(IOException e) {
+			System.out.println(e.getMessage());
+			}
+			catch (Exception e) {
+				System.out.println(e.getMessage());
+			}*/
+			try {
+				System.out.println(System.getProperty("user.dir"));
+				//System.out.println(new File("").getCanonicalPath());
+				File file = new File("src/it/meucci/prova2.txt");
+				file.createNewFile();
+				System.out.println("File creato");
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
 			aggiorna.start();
 		} catch (Exception e) {
 			// TODO: handle exception
