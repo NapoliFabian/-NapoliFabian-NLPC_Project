b7f5450c7d66fbaaf3233895d089fd01987daa94
NapoliFabian <fabiannapoli3bi@gmail.com>
2021-03-27 15:08:54 +0100

    Cambiati percorsi file properties

--- NLCP_gymapp2.0/src/it/meucci/GestoreFile.java
+++ NLCP_gymapp2.0/src/it/meucci/GestoreFile.java
@@ -12,31 +12,16 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.io.BufferedReader;
 public class GestoreFile {
-
+	private String default_config;
+	private String db_settings;
+	public GestoreFile() {
+		this.db_settings ="C:\\Users\\thema\\git\\-NapoliFabian-NLPC_Project\\NLCP_gymapp2.0\\config\\dbsettings.properties";
+		this.default_config ="C:\\Users\\thema\\git\\-NapoliFabian-NLPC_Project\\NLCP_gymapp2.0\\config\\defaultconf.properties";
+	}
 	
 	public String[] DBSettings() throws IOException {
-		/*try (OutputStream output = new FileOutputStream("configapp.properties")) {
-		Properties prop = new Properties();
-		prop.setProperty("db.url", "jdbc:mysql://localhost:3306/gym?serverTimezone=UTC");
-	    prop.setProperty("db.user", "root");
-	    prop.setProperty("db.password", "");
-		prop.store(output,null);
-		
-		}catch (Exception e) {
-			System.out.println(e.getMessage()+e.getStackTrace()+e.getCause()+e.getLocalizedMessage());
-		}*/
-		// Properties prop = readPropertiesFile("C:\\Users\\thema\\git\\-NapoliFabian-NLPC_Project\\NLCP_gymapp2.0\\configapp.properties");
 		 System.out.println("Leggo dati del db.....");
-		/* InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-         Stream<String> streamOfString= new BufferedReader(inputStreamReader).lines();
-         String streamToString = streamOfString.collect(Collectors.joining());
-         System.out.println(streamOfString.toString());*/
-		 String path ="C:\\Users\\thema\\git\\-NapoliFabian-NLPC_Project\\NLCP_gymapp2.0\\config\\dbsettings.properties";
-		 Properties prop = readPropertiesFile(path);
-	     /* System.out.println("username: "+ prop.getProperty("IP_DB"));
-	      System.out.println("user: "+ prop.getProperty("USER_DB"));
-	      System.out.println("password: "+ prop.getProperty("PW_DB"));*/
-	    // String[] dati = (prop.getProperty("IP_DB"), prop.getProperty("USER_DB"),prop.getProperty("PW_DB"));
+		 Properties prop = readPropertiesFile(db_settings);
 	      String[] dati;
 	      dati = new String[3];
 	      dati[0] = prop.getProperty("IP_DB");
