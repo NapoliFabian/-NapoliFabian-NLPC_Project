a91c75c0285cc5950547d2ff5ed953ca36da7678
NapoliFabian <fabiannapoli3bi@gmail.com>
2021-04-09 18:00:49 +0200

    Sistemazione tabelle e nuova icona app

--- NLCP_gymapp2.0/src/it/meucci/GestoreFile.java
+++ NLCP_gymapp2.0/src/it/meucci/GestoreFile.java
@@ -19,17 +19,16 @@ public class GestoreFile {
 
 
 
-		this.db_settings ="C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project000\\NLCP_gymapp2.0\\config\\dbsettings.properties";
-		this.default_config ="C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project000\\NLCP_gymapp2.0\\config\\defaultconf.properties";
+	//	this.db_settings ="C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project000\\NLCP_gymapp2.0\\config\\dbsettings.properties";
+	//	this.default_config ="C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project000\\NLCP_gymapp2.0\\config\\defaultconf.properties";
 		
 		
 		
 		/*this.db_settings ="C:\\Users\\thema\\git\\-NapoliFabian-NLPC_Project\\NLCP_gymapp2.0\\config\\dbsettings.properties";
 */
-/*
-		this.db_settings ="C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project20\\NLCP_gymapp2.0\\config\\dbsettings.properties";
-		this.default_config ="C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project20\\NLCP_gymapp2.0\\config\\defaultconf.properties";
-	*/	
+		this.db_settings = "C:\\Users\\thema\\Desktop\\progetti\\java eclipse napoli\\-NapoliFabian-NLPC_Project\\NLCP_gymapp2.0\\config\\dbsettings.properties";
+		this.default_config = "C:\\Users\\thema\\Desktop\\progetti\\java eclipse napoli\\-NapoliFabian-NLPC_Project\\NLCP_gymapp2.0\\config\\defaultconf.properties";
+		
 		
 	}
 	public String[] getConfig() throws IOException {
@@ -84,9 +83,9 @@ public class GestoreFile {
 		if(language==null)
 			language = "ing";
 		
-	//	Properties prop = readLanguage("C:\\Users\\thema\\Desktop\\progetti\\java eclipse napoli\\-NapoliFabian-NLPC_Project\\NLCP_gymapp2.0\\config\\Language\\"+language+".properties");
+		Properties prop = readLanguage("C:\\Users\\thema\\Desktop\\progetti\\java eclipse napoli\\-NapoliFabian-NLPC_Project\\NLCP_gymapp2.0\\config\\Language\\"+language+".properties");
 	//	Properties prop = readLanguage("C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project20\\NLCP_gymapp2.0\\config\\Language\\"+language+".properties");
-			Properties prop = readLanguage("C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project000\\NLCP_gymapp2.0\\config\\Language\\"+language+".properties");// simone
+		//	Properties prop = readLanguage("C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project000\\NLCP_gymapp2.0\\config\\Language\\"+language+".properties");// simone
 		String[] config;
 		config = new String[max];
 		config[0] = prop.getProperty("WELCOME_MESSAGE");
