78a789ff8b668a66b81c0d19c76dc2be06ac439f
Longo <user@MMM-PC>
2021-04-28 17:35:26 +0200

    sistemati file properties

--- GYMAPP/src/it/meucci/GestoreFile.java
+++ GYMAPP/src/it/meucci/GestoreFile.java
@@ -17,82 +17,27 @@ public class GestoreFile {
 	public GestoreFile() {
 
 
-
-
+		//CATALDO
 		//this.db_settings ="C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\dbsettings.properties";
 		//this.default_config ="C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\defaultconf.properties";
 
-
- //CATALDO
-<<<<<<< HEAD
-		this.db_settings ="C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\dbsettings.properties";
-		this.default_config ="C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\defaultconf.properties";
-=======
-<<<<<<< HEAD
-		/*this.db_settings ="C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\dbsettings.properties";
-		this.default_config ="C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\defaultconf.properties";
-=======
-		//this.db_settings ="C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\dbsettings.properties";
-	//	this.default_config ="C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\defaultconf.properties";
->>>>>>> branch 'master30' of https://github.com/NapoliFabian/-NapoliFabian-NLPC_Project.git
->>>>>>> branch 'master30' of https://github.com/NapoliFabian/-NapoliFabian-NLPC_Project.git
 		
-		*/
-// 	NAPOLI
-
-		/* this.db_settings = "C:\\Users\\thema\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\dbsettings.properties";
-		 this.default_config = "C:\\Users\\thema\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\defaultconf.properties";*/
-
-		 //this.db_settings = "C:\\Users\\thema\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\dbsettings.properties";
+		//NAPOLI
+		//this.db_settings = "C:\\Users\\thema\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\dbsettings.properties";
 		// this.default_config = "C:\\Users\\thema\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\defaultconf.properties";
-//github.com/NapoliFabian/-NapoliFabian-NLPC_Project.git
 
-
-/*
-		this.db_settings ="C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\dbsettings.properties";
-		this.default_config ="C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\defaultconf.properties";
-*/
-       //SIMONE
-/*
-		this.db_settings ="C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\dbsettings.properties";
-		this.default_config ="C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\defaultconf.properties";
-*/
-	/*	this.db_settings ="C:\\Users\\Samuel\\Desktop\\eclipse\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\dbsettings.properties";
-		this.default_config ="C:\\Users\\Samuel\\Desktop\\eclipse\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\defaultconf.properties";*/
 	
-		//FABIAN
-		/* this.db_settings = "C:\\Users\\thema\\Desktop\\progetti\\java eclipse napoli\\-NapoliFabian-NLPC_Project\\NLCP_gymapp2.0\\config\\dbsettings.properties";
-		 this.default_config = "C:\\Users\\thema\\Desktop\\progetti\\java eclipse napoli\\-NapoliFabian-NLPC_Project\\NLCP_gymapp2.0\\config\\defaultconf.properties";*/
 
+		//SAMUEL
+		//this.db_settings ="C:\\Users\\Samuel\\Desktop\\eclipse\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\dbsettings.properties";
+		//this.default_config ="C:\\Users\\Samuel\\Desktop\\eclipse\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\defaultconf.properties";
+	
+	
 
-
-/*      LONGO*/
-<<<<<<< HEAD
-
-		 /*
-=======
-
-<<<<<<< HEAD
-		//this.db_settings ="C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\dbsettings.properties";
-		//this.default_config ="C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\defaultconf.properties";
-=======
->>>>>>> branch 'master30' of https://github.com/NapoliFabian/-NapoliFabian-NLPC_Project.git
+		//LONGO
 		this.db_settings ="C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\dbsettings.properties";
-<<<<<<< HEAD
-
-		 
-		/*this.db_settings ="C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\dbsettings.properties";
-
-		this.default_config ="C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\defaultconf.properties";*/
-=======
 		this.default_config ="C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\defaultconf.properties";
->>>>>>> branch 'master30' of https://github.com/NapoliFabian/-NapoliFabian-NLPC_Project.git
->>>>>>> branch 'master30' of https://github.com/NapoliFabian/-NapoliFabian-NLPC_Project.git
 
-	/*	SAMUEL
-		this.db_settings ="C:\\Users\\Samuel\\Desktop\\eclipse\\-NapoliFabian-NLPC_Project\\NLCP_gymapp2.0\\config\\dbsettings.properties";
-		this.default_config ="C:\\Users\\Samuel\\Desktop\\eclipse\\-NapoliFabian-NLPC_Project\\NLCP_gymapp2.0\\config\\defaultconf.properties";
-*/
 	}
 	public String[] getConfig() throws IOException {
 		System.out.println("Leggo dati conf");
@@ -153,56 +98,27 @@ public class GestoreFile {
 
 	
 		//LONGO
-
-<<<<<<< HEAD
-	//Properties prop = readLanguage("C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\Language\\"+language+".properties");
-=======
-
-<<<<<<< HEAD
-	//Properties prop = readLanguage("C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\Language\\"+language+".properties");
-=======
-	Properties prop = readLanguage("C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\Language\\"+language+".properties");
->>>>>>> branch 'master30' of https://github.com/NapoliFabian/-NapoliFabian-NLPC_Project.git
->>>>>>> branch 'master30' of https://github.com/NapoliFabian/-NapoliFabian-NLPC_Project.git
-
-//	Properties prop = readLanguage("C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\Language\\"+language+".properties");
-<<<<<<< HEAD
-
-=======
-//github.com/NapoliFabian/-NapoliFabian-NLPC_Project.git
->>>>>>> branch 'master30' of https://github.com/NapoliFabian/-NapoliFabian-NLPC_Project.git
-		//SIMONE
-	//	Properties prop = readLanguage("C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project000\\NLCP_gymapp2.0\\config\\Language\\"+language+".properties");// simone
-
-	//	Properties prop = readLanguage("C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project20\\NLCP_gymapp2.0\\config\\Language\\"+language+".properties");
+		Properties prop = readLanguage("C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\Language\\"+language+".properties");
+		
+		
 		//FABIAN
-	//Properties prop = readLanguage("C:\\Users\\thema\\Desktop\\progetti\\java eclipse napoli\\-NapoliFabian-NLPC_Project\\NLCP_gymapp2.0\\config\\Language\\"+language+".properties");
+		//Properties prop = readLanguage("C:\\Users\\thema\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\Language\\"+language+".properties");
 
+		
 		//SIMONE
-	//	Properties prop = readLanguage("C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project000\\NLCP_gymapp2.0\\config\\Language\\"+language+".properties");// simone
-		
-		
-	//FABIAN
-	//Properties prop = readLanguage("C:\\Users\\thema\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\Language\\"+language+".properties");
-
-			//SIMONE
-		Properties prop = readLanguage("C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\Language\\"+language+".properties");// simone
-
-
-
-
-
 		//Properties prop = readLanguage("C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\Language\\"+language+".properties");// simone
 
 
-			//SIMONE
-	//Properties prop = readLanguage("C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\Language\\"+language+".properties");// simone
-	//SAMUEL
-
+		//SAMUEL
 		//Properties prop = readLanguage("C:\\Users\\Samuel\\Desktop\\eclipse\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\Language"+"\\ing"+".properties"); //Samuel
 
-	//	Properties prop = readLanguage("C:\\Users\\Samuel\\Desktop\\eclipse\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\Language"+"\\ing"+".properties"); //Samuel
 
+		
+
+			
+
+		
+	
 	
 		String[] config;
 		config = new String[max];
