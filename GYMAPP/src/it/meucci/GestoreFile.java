7e46b1d7cd4a2d5bac27d3b41112eeb741c4e618
Longo <user@MMM-PC>
2021-04-28 16:53:49 +0200

    sistemato metodo rinnovaAbbonamento

--- GYMAPP/src/it/meucci/GestoreFile.java
+++ GYMAPP/src/it/meucci/GestoreFile.java
@@ -24,8 +24,8 @@ public class GestoreFile {
 
 
  //CATALDO
-		this.db_settings ="C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\dbsettings.properties";
-		this.default_config ="C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\defaultconf.properties";
+		//this.db_settings ="C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\dbsettings.properties";
+	//	this.default_config ="C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\defaultconf.properties";
 		
 		
 // 	NAPOLI
@@ -58,13 +58,8 @@ public class GestoreFile {
 
 /*      LONGO*/
 
-		 /*
 		this.db_settings ="C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\dbsettings.properties";
-=======
-		 
-		/*this.db_settings ="C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\dbsettings.properties";
->>>>>>> branch 'master30' of https://github.com/NapoliFabian/-NapoliFabian-NLPC_Project.git
-		this.default_config ="C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\defaultconf.properties";*/
+		this.default_config ="C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\defaultconf.properties";
 
 	/*	SAMUEL
 		this.db_settings ="C:\\Users\\Samuel\\Desktop\\eclipse\\-NapoliFabian-NLPC_Project\\NLCP_gymapp2.0\\config\\dbsettings.properties";
@@ -132,7 +127,7 @@ public class GestoreFile {
 		//LONGO
 
 
-	//Properties prop = readLanguage("C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\Language\\"+language+".properties");
+	Properties prop = readLanguage("C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\Language\\"+language+".properties");
 
 //	Properties prop = readLanguage("C:\\Users\\user\\git\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\Language\\"+language+".properties");
 //github.com/NapoliFabian/-NapoliFabian-NLPC_Project.git
@@ -161,7 +156,7 @@ public class GestoreFile {
 
 
 			//SIMONE
-	Properties prop = readLanguage("C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\Language\\"+language+".properties");// simone
+	//Properties prop = readLanguage("C:\\Users\\CATALDO SIMONE\\git\\-NapoliFabian-NLPC_Project00\\GYMAPP\\config\\Language\\"+language+".properties");// simone
 	//SAMUEL
 
 		//Properties prop = readLanguage("C:\\Users\\Samuel\\Desktop\\eclipse\\-NapoliFabian-NLPC_Project\\GYMAPP\\config\\Language"+"\\ing"+".properties"); //Samuel
